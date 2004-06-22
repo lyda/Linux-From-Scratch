ab1544b7b53f8994f2cf4414e2374f8e311e8795
cvs2svn <cvs2svn@migration>
2004-06-22 19:48:02 +0000

    This commit was manufactured by cvs2svn to create tag 'v5_1-pre2'.
    git-svn-id: svn://svn.linuxfromscratch.org/LFS/tags/v5_1-pre2@3453 4aa44e1e-78dd-0310-a6d2-fbcd4c07a689

--- lfscmd/src/env.h
+++
@@ -1,6 +0,0 @@
-#ifndef ENV_H
-#define ENV_H
-
-extern char *locate_book (const char *varname);
-
-#endif
--- lfscmd/src/file.h
+++
@@ -1,6 +0,0 @@
-#ifndef FILE_H
-#define FILE_H
-
-extern FILE *write_file (const char *fname, const char *mode);
-
-#endif
